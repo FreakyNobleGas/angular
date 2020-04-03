231a09d48992be599dedea414d2cce94c74efb1c
Nick Quinn <nickquinn3131@gmail.com>
2020-04-03 16:18:33 -0400

    Implemented HostBinding and HostListener. HostBinding is a much more efficient approach.

--- Udemy-Angular-The-Complete-Guide-2020/Directives-Deep-Dive/src/app/better-highlight/better-highlight.directive.ts
+++ Udemy-Angular-The-Complete-Guide-2020/Directives-Deep-Dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,27 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
     selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+    // Binds to the host element
+    @HostBinding('style.backgroundColor') backgroundColor : string = 'transparent';
     
     constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
-        // render takes a html element, a style, and the style attribute
-        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+        // render takes a html element, a style, and the style attribute. There are many more methods in renderer other than setStyle.
+        // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     }
 
+    // mouseenter and mouseleave are built-in functions for HostListener
+    @HostListener('mouseenter') mouseover(data : Event) {
+        //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+        this.backgroundColor = 'blue';
+    }
+
+    @HostListener('mouseleave') mouseleave(data : Event) {
+        //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+        this.backgroundColor = 'transparent';
+    }
 }
